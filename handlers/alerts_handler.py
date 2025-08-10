718e68b830a11dcc751e5c737787493a3d05492a
dort5bot <dersdep@gmail.com>
2025-08-10 22:33:34 +0300

    Create alerts_handler.py

---
+++ handlers/alerts_handler.py
@@ -0,0 +1,16 @@
+# handlers/alerts_handler.py
+##♦️merkezi telegram alert wrapper
+
+import os
+from utils.monitoring import telegram_alert, configure_logging
+import logging
+
+LOG = logging.getLogger("alerts_handler")
+
+def alert_info(msg: str):
+    LOG.info("ALERT: %s", msg)
+    telegram_alert(msg)
+
+def alert_error(msg: str):
+    LOG.error("ALERT: %s", msg)
+    telegram_alert(msg)
