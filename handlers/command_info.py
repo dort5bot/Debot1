cd33407d652fa674b3301cf52c6d3d37af39001c
dort5bot <dersdep@gmail.com>
2025-08-21 07:56:49 +0300

    Update command_info.py

--- handlers/command_info.py
+++ handlers/command_info.py
@@ -16,4 +16,5 @@ COMMAND_INFO = {
     "p_sil": "Favori coin listesinden coin siler",
     "fr": "Funding Rate raporu ve günlük CSV kaydı",
     "whale": "Whale Alerts raporu ve günlük CSV kaydı"
+    "t":"/t: hazir liste, /t sayı: hacimli n tanesi, /t coin zaman: istedigin coine ve istenen zamana iat bilgi"
 }
